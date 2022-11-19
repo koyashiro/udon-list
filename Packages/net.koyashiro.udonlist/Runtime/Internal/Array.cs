cb9ba163746f982e4baa372204686bdfadc98d09
koyashiro <develop@koyashi.ro>
2022-11-20 06:30:44 +0900

    feat: implement internal `Array` class

---
+++ Packages/net.koyashiro.udonlist/Runtime/Internal/Array.cs
@@ -0,0 +1,99 @@
+namespace Koyashiro.UdonList.Internal
+{
+    public static class Array
+    {
+        public static int IndexOf(object[] array, object value, int startIndex, int count)
+        {
+            if (array == null)
+            {
+                UdonException.ThrowArgumentNullException(nameof(array));
+            }
+
+            if ((uint)startIndex > (uint)array.Length)
+            {
+                UdonException.ThrowArgumentOutOfRangeException();
+            }
+
+            if ((uint)count > (uint)(array.Length - startIndex))
+            {
+                UdonException.ThrowArgumentOutOfRangeException();
+            }
+
+            for (var i = startIndex; i < startIndex + count; i++)
+            {
+                if (object.Equals(array[i], value))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int LastIndexOf(object[] array, object value, int startIndex, int count)
+        {
+            if (array == null)
+            {
+                UdonException.ThrowArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                if (startIndex != -1 && startIndex != 0)
+                {
+                    UdonException.ThrowArgumentOutOfRangeException();
+                }
+
+                if (count != 0)
+                {
+                    UdonException.ThrowArgumentOutOfRangeException();
+                }
+
+                return -1;
+            }
+
+            if ((uint)startIndex >= (uint)array.Length)
+            {
+                UdonException.ThrowArgumentOutOfRangeException();
+            }
+
+            if (count < 0 || startIndex - count + 1 < 0)
+            {
+                UdonException.ThrowArgumentOutOfRangeException();
+            }
+
+            for (var i = 0; i < count; i++)
+            {
+                if (object.Equals(array[startIndex - i], value))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static void Copy<TS, TD>(TS[] sourceArray, TD[] destinationArray, int length)
+        {
+            Copy(sourceArray, 0, destinationArray, 0, length);
+        }
+
+        public static void Copy<TS, TD>(TS[] sourceArray, int sourceIndex, TD[] destinationArray, int destinationIndex, int length)
+        {
+            if (sourceArray == null)
+            {
+                UdonException.ThrowArgumentNullException(nameof(sourceArray));
+            }
+
+            if (sourceArray == null)
+            {
+                UdonException.ThrowArgumentNullException(nameof(sourceArray));
+            }
+
+            for (var i = 0; i < length; i++)
+            {
+                destinationArray[destinationIndex + i] = (TD)(object)(sourceArray[sourceIndex + i]);
+            }
+        }
+    }
+}
