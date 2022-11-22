f7f8fe1c3e5765d93088f5d17326d2b0eac87fe3
koyashiro <develop@koyashi.ro>
2022-11-22 22:19:14 +0900

    refacor: inline function calls

--- Packages/net.koyashiro.udonlist/Runtime/Internal/UdonList.cs
+++ Packages/net.koyashiro.udonlist/Runtime/Internal/UdonList.cs
@@ -8,7 +8,10 @@ namespace Koyashiro.UdonList.Internal
 
         public static object[] New()
         {
-            return New(0);
+            var items = new object[0];
+            var size = 0;
+
+            return new object[] { items, size };
         }
 
         public static object[] New<T>(T[] collection)
@@ -19,11 +22,13 @@ namespace Koyashiro.UdonList.Internal
             }
 
             var items = new object[collection.Length];
+            var size = collection.Length;
+
             if (collection.Length > 0)
             {
                 Array.Copy(collection, items, items.Length);
             }
-            var size = collection.Length;
+
             return new object[] { items, size };
         }
 
@@ -36,49 +41,41 @@ namespace Koyashiro.UdonList.Internal
 
             var items = new object[capacity];
             var size = 0;
+
             return new object[] { items, size };
-        }
-
-        private static object[] Items(object[] list)
-        {
-            return (object[])list[0];
-        }
-
-        private static void SetItems(object[] list, object[] items)
-        {
-            list[0] = items;
         }
 
         public static int Capacity(object[] list)
         {
-            return Items(list).Length;
+            var items = (object[])list[0];
+
+            return items.Length;
         }
 
         public static void SetCapacity(object[] list, int capacity)
         {
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if (capacity < size)
             {
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = Items(list);
-            if (capacity != items.Length)
+            if (capacity == items.Length)
             {
-                if (capacity > 0)
-                {
-                    var newItems = new object[capacity];
-                    if (capacity > 0)
-                    {
-                        Array.Copy(items, newItems, size);
-                    }
-                    SetItems(list, newItems);
-                }
-                else
-                {
-                    SetItems(list, new object[0]);
-                }
+                return;
             }
+
+            if (capacity == 0)
+            {
+                list[0] = new object[0];
+                return;
+            }
+
+            var newItems = new object[capacity];
+            Array.Copy(items, newItems, size);
+            list[0] = newItems;
         }
 
         public static int Count(object[] list)
@@ -93,48 +90,43 @@ namespace Koyashiro.UdonList.Internal
 
         public static T GetItem<T>(object[] list, int index)
         {
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if ((uint)index >= (uint)size)
             {
                 UdonException.ThrowIndexOutOfRangeException();
             }
 
-            return (T)Items(list)[index];
+            return (T)items[index];
         }
 
         public static void SetItem<T>(object[] list, int index, T item)
         {
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if ((uint)index >= (uint)size)
             {
                 UdonException.ThrowIndexOutOfRangeException();
             }
 
-            var items = Items(list);
             items[index] = item;
         }
 
         public static void Add<T>(object[] list, T item)
         {
-            var size = Count(list);
-            var items = Items(list);
-            if ((uint)size < (uint)items.Length)
-            {
-                SetCount(list, size + 1);
-                SetItem(list, size, item);
-            }
-            else
-            {
-                AddWithResize(list, item);
-            }
-        }
+            var items = (object[])list[0];
+            var size = (int)list[1];
 
-        private static void AddWithResize<T>(object[] list, T item)
-        {
-            var size = Count(list);
-            Grow(list, size + 1);
-            SetCount(list, size + 1);
-            SetItem(list, size, item);
+            if ((uint)size >= (uint)items.Length)
+            {
+                Grow(list, size + 1);
+                items = (object[])list[0];
+            }
+
+            items[size] = item;
+            list[1] = size + 1;
         }
 
         public static void AddRange<T>(object[] list, T[] collection)
@@ -144,30 +136,33 @@ namespace Koyashiro.UdonList.Internal
                 UdonException.ThrowArgumentNullException(nameof(collection));
             }
 
-            if (collection.Length > 0)
+            if (collection.Length == 0)
             {
-                var items = Items(list);
-                var size = Count(list);
-                if (items.Length - size < collection.Length)
-                {
-                    Grow(list, size + items.Length);
-                }
-
-                items = Items(list);
-                Array.Copy(collection, items, collection.Length);
-                SetCount(list, size + items.Length);
+                return;
             }
+
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
+            if (items.Length - size < collection.Length)
+            {
+                Grow(list, size + items.Length);
+                items = (object[])list[0];
+            }
+
+            Array.Copy(collection, items, collection.Length);
+            list[1] = size + items.Length;
         }
 
         public static void Clear(object[] list)
         {
-            int size = Count(list);
-            SetCount(list, 0);
+            list[1] = 0;
         }
 
         public static bool Contains<T>(object[] list, T item)
         {
-            var size = Count(list);
+            var size = (int)list[1];
+
             return size != 0 && IndexOf(list, item) >= 0;
         }
 
@@ -183,13 +178,14 @@ namespace Koyashiro.UdonList.Internal
                 UdonException.ThrowArgumentNullException(nameof(array));
             }
 
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if (size - index < count)
             {
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = Items(list);
             Array.Copy(items, index, array, arrayIndex, count);
         }
 
@@ -200,8 +196,9 @@ namespace Koyashiro.UdonList.Internal
                 UdonException.ThrowArgumentNullException(nameof(array));
             }
 
-            var items = Items(list);
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             Array.Copy(items, 0, array, arrayIndex, size);
         }
 
@@ -212,10 +209,12 @@ namespace Koyashiro.UdonList.Internal
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = Items(list);
+            var items = (object[])list[0];
+
             if (items.Length > capacity)
             {
                 Grow(list, capacity);
+                items = (object[])list[0];
             }
 
             return items.Length;
@@ -223,7 +222,7 @@ namespace Koyashiro.UdonList.Internal
 
         private static void Grow(object[] list, int capacity)
         {
-            var items = Items(list);
+            var items = (object[])list[0];
             int newCapacity = items.Length == 0 ? DEFAULT_CAPACITY : items.Length * 2;
 
             if (newCapacity < capacity)
@@ -246,18 +245,18 @@ namespace Koyashiro.UdonList.Internal
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if (size - index < count)
             {
                 UdonException.ThrowArgumentException();
             }
 
-            var items = Items(list);
-            var newList = New(count);
-            var newListItems = Items(newList);
-            Array.Copy(items, index, newListItems, 0, count);
-            SetCount(newList, count);
-            return newList;
+            var newItems = new object[count];
+            Array.Copy(items, index, newItems, 0, count);
+
+            return new object[] { newItems, count };
         }
 
         public static object[] Slice(object[] list, int start, int length)
@@ -267,26 +266,30 @@ namespace Koyashiro.UdonList.Internal
 
         public static int IndexOf<T>(object[] list, T item)
         {
-            var items = Items(list);
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             return Array.IndexOf(items, item, 0, size);
         }
 
         public static int IndexOf<T>(object[] list, T item, int index)
         {
-            if (index > Count(list))
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
+            if (index > size)
             {
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = Items(list);
-            var size = Count(list);
             return Array.IndexOf(items, item, index, size - index);
         }
 
         public static int IndexOf<T>(object[] list, T item, int index, int count)
         {
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if (index > size)
             {
                 UdonException.ThrowArgumentOutOfRangeException();
@@ -297,31 +300,32 @@ namespace Koyashiro.UdonList.Internal
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = Items(list);
             return Array.IndexOf(items, item, index, count);
         }
 
         public static void Insert<T>(object[] list, int index, T item)
         {
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if ((uint)index > (uint)size)
             {
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = Items(list);
             if (size == items.Length)
             {
                 Grow(list, size + 1);
-                items = Items(list);
+                items = (object[])list[0];
             }
 
             if (index < size)
             {
                 Array.Copy(items, index, items, index + 1, size - index);
             }
+
             items[index] = item;
-            SetCount(list, size + 1);
+            list[1] = size + 1;
         }
 
         public static void InsertRange<T>(object[] list, int index, T[] collection)
@@ -331,40 +335,46 @@ namespace Koyashiro.UdonList.Internal
                 UdonException.ThrowArgumentNullException(nameof(collection));
             }
 
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if ((uint)index > (uint)size)
             {
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            if (collection.Length > 0)
+            if (collection.Length <= 0)
             {
-                var items = Items(list);
-                if (items.Length - size < collection.Length)
-                {
-                    Grow(list, size + collection.Length);
-                }
-
-                if (index < size)
-                {
-                    Array.Copy(items, index, items, index + collection.Length, size - index);
-                }
-
-                if (object.Equals(list, collection))
-                {
-                    Array.Copy(items, 0, items, index, index);
-                    Array.Copy(items, index + collection.Length, items, index * 2, size - index);
-                }
-                {
-                    Array.Copy(collection, items, index);
-                }
-                SetCount(list, collection.Length);
+                return;
             }
+
+            if (items.Length - size < collection.Length)
+            {
+                Grow(list, size + collection.Length);
+                items = (object[])list[0];
+            }
+
+            if (index < size)
+            {
+                Array.Copy(items, index, items, index + collection.Length, size - index);
+            }
+
+            if (object.Equals(list, collection))
+            {
+                Array.Copy(items, 0, items, index, index);
+                Array.Copy(items, index + collection.Length, items, index * 2, size - index);
+            }
+            else
+            {
+                Array.Copy(collection, items, index);
+            }
+
+            list[1] = collection.Length;
         }
 
         public static int LastIndexOf<T>(object[] list, T item)
         {
-            var size = Count(list);
+            var size = (int)list[1];
             if (size == 0)
             {
                 return -1;
@@ -375,7 +385,7 @@ namespace Koyashiro.UdonList.Internal
 
         public static int LastIndexOf<T>(object[] list, T item, int index)
         {
-            var size = Count(list);
+            var size = (int)list[1];
             if (index >= size)
             {
                 UdonException.ThrowArgumentOutOfRangeException();
@@ -386,7 +396,9 @@ namespace Koyashiro.UdonList.Internal
 
         public static int LastIndexOf<T>(object[] list, T item, int index, int count)
         {
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if ((size != 0) && (index < 0))
             {
                 UdonException.ThrowArgumentOutOfRangeException();
@@ -412,7 +424,6 @@ namespace Koyashiro.UdonList.Internal
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = Items(list);
             return Array.LastIndexOf(items, item, index, count);
         }
 
@@ -430,18 +441,22 @@ namespace Koyashiro.UdonList.Internal
 
         public static void RemoveAt(object[] list, int index)
         {
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if ((uint)index >= (uint)size)
             {
                 UdonException.ThrowArgumentOutOfRangeException();
             }
+
             size--;
+
             if (index < size)
             {
-                var items = Items(list);
                 Array.Copy(items, index + 1, items, index, size - index);
             }
-            SetCount(list, size);
+
+            list[1] = size;
         }
 
         public static void RemoveRange(object[] list, int index, int count)
@@ -456,47 +471,57 @@ namespace Koyashiro.UdonList.Internal
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if (size - index < count)
             {
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            if (count > 0)
+            if (count <= 0)
             {
-                size -= count;
-                var items = Items(list);
-                if (index < size)
-                {
-                    Array.Copy(items, index + count, items, index, size - index);
-                }
-                SetCount(list, size);
+                return;
             }
+
+            size -= count;
+            if (index < size)
+            {
+                Array.Copy(items, index + count, items, index, size - index);
+            }
+
+            list[1] = size;
         }
 
         public static T[] ToArray<T>(object[] list)
         {
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             if (size == 0)
             {
                 return new T[0];
             }
 
             var array = new T[size];
-            var items = Items(list);
             Array.Copy(items, array, size);
+
             return array;
         }
 
         public static void TrimExcess(object[] list)
         {
-            var items = Items(list);
-            var size = Count(list);
+            var items = (object[])list[0];
+            var size = (int)list[1];
+
             int threshold = (int)(((double)items.Length) * 0.9);
+
             if (size < threshold)
             {
-                SetCapacity(list, size);
+                return;
             }
+
+            SetCapacity(list, size);
         }
     }
 }
