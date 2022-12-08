85b6f9aed72e879a8d63d67845263d14028bb76f
ureishi <victorique2020+github@gmail.com>
2022-12-08 17:05:49 +0900

    UdonGenericListとそのテストを追加します
    ・UdonGenericListとそのテストを追加します。

---
+++ Packages/net.koyashiro.udonlist/Tests/Runtime/UdonGenericListTest.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UdonSharp;
+using Koyashiro.UdonTest;
+
+namespace Koyashiro.UdonList.Tests
+{
+    using Koyashiro.UdonList;
+
+    [AddComponentMenu("")]
+    public class UdonGenericListTest : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private GameObject[] gameObjects = { };
+
+        public void Start()
+        {
+            var gameObjectList = UdonGenericList.New<GameObject>();
+            gameObjectList.Add(gameObjects[0]);
+            gameObjectList.Add(gameObjects[1]);
+            gameObjectList.Add(gameObjects[2]);
+            Assert.Equal(gameObjects[0], gameObjectList.GetItem<GameObject>(0));
+            Assert.Equal(gameObjects[1], gameObjectList.GetItem<GameObject>(1));
+            Assert.Equal(gameObjects[2], gameObjectList.GetItem<GameObject>(2));
+            Assert.True(gameObjectList.Remove(gameObjects[1]));
+            Assert.False(gameObjectList.Remove(gameObjects[3]));
+            Assert.Equal(gameObjects[0], gameObjectList.GetItem<GameObject>(0));
+            Assert.Equal(gameObjects[2], gameObjectList.GetItem<GameObject>(1));
+            gameObjectList.SetItem(1, gameObjects[1]);
+            Assert.Equal(gameObjects[1], gameObjectList.GetItem<GameObject>(1));
+
+            gameObjectList = UdonGenericList.New(gameObjects);
+            gameObjectList.Reverse();
+            for (var i = 0; i < gameObjects.Length; i++)
+            {
+                Assert.Equal(gameObjects[gameObjects.Length - 1 - i], gameObjectList.GetItem<GameObject>(i));
+            }
+        }
+    }
+}
