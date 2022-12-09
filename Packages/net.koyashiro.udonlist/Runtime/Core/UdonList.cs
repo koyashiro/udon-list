9bfa2015d609812746e1901f5021ddf5932fd725
ureishi <victorique2020+github@gmail.com>
2022-12-09 11:53:11 +0900

    キャストの修正とサンプルのコメントの細かい修正
    - 適切でないキャストを修正します
    `var items = (object[])list[0];` -> `var items = (Array)list[0];`

--- Packages/net.koyashiro.udonlist/Runtime/Core/UdonList.cs
+++ Packages/net.koyashiro.udonlist/Runtime/Core/UdonList.cs
@@ -122,7 +122,7 @@ namespace Koyashiro.UdonList.Core
             if ((uint)size >= (uint)items.Length)
             {
                 Grow(list, size + 1);
-                items = (T[])list[0];
+                items = (Array)list[0];
             }
 
             items.SetValue(item, size);
@@ -307,7 +307,7 @@ namespace Koyashiro.UdonList.Core
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = (T[])list[0];
+            var items = (Array)list[0];
 
             return Array.IndexOf(items, item, index, count);
         }
@@ -321,12 +321,12 @@ namespace Koyashiro.UdonList.Core
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = (T[])list[0];
+            var items = (Array)list[0];
 
             if (size == items.Length)
             {
                 Grow(list, size + 1);
-                items = (T[])list[0];
+                items = (Array)list[0];
             }
 
             if (index < size)
@@ -532,7 +532,7 @@ namespace Koyashiro.UdonList.Core
                 UdonException.ThrowArgumentOutOfRangeException();
             }
 
-            var items = (object[])list[0];
+            var items = (Array)list[0];
 
             Array.Reverse(items, index, count);
         }
@@ -570,7 +570,7 @@ namespace Koyashiro.UdonList.Core
 
         public static T[] ToArray<T>(object[] list)
         {
-            var items = (object[])list[0];
+            var items = (Array)list[0];
             var size = (int)list[1];
 
             if (size == 0)
@@ -586,7 +586,7 @@ namespace Koyashiro.UdonList.Core
 
         public static void TrimExcess(object[] list)
         {
-            var items = (object[])list[0];
+            var items = (Array)list[0];
             var size = (int)list[1];
 
             int threshold = (int)(((double)items.Length) * 0.9);
